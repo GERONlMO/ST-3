d88a24c647e975e1c2d1f4ee250f4f51a5984ab8
Ivan Novostroev <54308529+GERONlMO@users.noreply.github.com>
2024-04-07 19:46:16 +0300

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,12 +6,12 @@
 #include "TimedDoor.h"
 
 class MockTimerClient : public TimerClient {
-public:
+ public:
     MOCK_METHOD(void, Timeout, (), (override));
 };
 
 class MockDoor : public Door {
-public:
+ public:
     MOCK_METHOD(void, lock, (), (override));
     MOCK_METHOD(void, unlock, (), (override));
     MOCK_METHOD(bool, isDoorOpened, (), (override));
